e06ce1fb2752f50e3298147013718bfcd21d8efa
jarocode <okolieuchenna890@gmail.com>
2025-04-30 15:05:19 +0100

    env configuration

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    AuthModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env.local', '.env.development', '.env.production'],
+    }),
+  ],
   controllers: [],
   providers: [],
 })
