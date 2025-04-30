268a00d217d8b03f36cd4ee24d33d16a71cde21b
jarocode <okolieuchenna890@gmail.com>
2025-04-30 15:49:00 +0100

    shop module

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
 import { AuthModule } from './auth/auth.module';
+import { ShopModule } from './shop/shop.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { AuthModule } from './auth/auth.module';
       isGlobal: true,
       envFilePath: ['.env.local', '.env.development', '.env.production'],
     }),
+    ShopModule,
   ],
   controllers: [],
   providers: [],
