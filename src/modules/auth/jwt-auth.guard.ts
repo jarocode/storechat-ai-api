3358710a97d8ee31dc612b18fc4aaa8841c0b5bb
jarocode <okolieuchenna890@gmail.com>
2025-05-06 13:35:59 +0100

    chore: logged token verification in jwtAuthGuard

--- src/modules/auth/jwt-auth.guard.ts
+++ src/modules/auth/jwt-auth.guard.ts
@@ -4,23 +4,28 @@ import {
   CanActivate,
   ExecutionContext,
   UnauthorizedException,
+  Logger,
 } from '@nestjs/common';
 import { JwtService, TokenExpiredError } from '@nestjs/jwt';
 import type { Request, Response } from 'express';
 
 @Injectable()
 export class JwtAuthGuard implements CanActivate {
+  private readonly logger = new Logger(JwtAuthGuard.name);
   constructor(private readonly jwtService: JwtService) {}
 
   canActivate(ctx: ExecutionContext): boolean {
     const req = ctx.switchToHttp().getRequest<Request>();
     const res = ctx.switchToHttp().getResponse<Response>();
     const token = req.cookies?.jwt;
+    this.logger.log('jwt cookie token:', token);
     if (!token) throw new UnauthorizedException('Not authenticated');
 
     try {
+      this.logger.log('verifying jwt token...');
       const payload = this.jwtService.verify<{ shop: string }>(token);
       (req as any).shop = payload.shop;
+      this.logger.log('jwt token verified successfully');
       return true;
     } catch (err) {
       // If it’s an expired token, clear the cookie so the client stops sending it
